1d4b8bdf9c2726d417621de43d5b982892d0750a
Jim Lavin <ilovehamradio@gmail.com>
2023-08-23 18:03:41 -0500

    initial code for project

---
+++ SRC/ESP32-DS1307-RTC/src/main.cpp
@@ -0,0 +1,55 @@
+#include <SPI.h>
+#include <RTClib.h>
+
+RTC_DS1307 rtc;
+
+char daysOfWeek[7][12] = {
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"};
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // SETUP RTC MODULE
+  if (!rtc.begin())
+  {
+    Serial.println("RTC module is NOT found");
+    Serial.flush();
+    while (1)
+      ;
+  }
+
+  // automatically sets the RTC to the date & time on PC this sketch was compiled
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  // manually sets the RTC with an explicit date & time, for example to set
+  // January 21, 2021 at 3am you would call:
+  // rtc.adjust(DateTime(2021, 1, 21, 3, 0, 0));
+}
+
+void loop()
+{
+  DateTime now = rtc.now();
+  Serial.print("ESP32 RTC Date Time: ");
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.println(now.second(), DEC);
+
+  delay(1000); // delay 1 seconds
+}
